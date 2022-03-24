5fd67bc4b32ef38973f6160effcc7c83b21fa465
Julian Schlenker <schlenkerjulian@outlook.com>
2022-03-24 23:42:56 +0100

    feat: Add Spacer

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export interface ButtonProps {
-    label: string;
+  label: string;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.label}</button>
-}
+  return <button>{props.label}</button>;
+};
 
 export default Button;
---
+++ src/components/Spacer/Spacer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export interface SpacerProps {
+  size?: number;
+  horizontal?: boolean;
+  flex?: number;
+}
+
+const Spacer: React.FunctionComponent<SpacerProps> = ({
+  flex,
+  size = 8,
+  horizontal = false,
+}) => {
+  if (flex == null) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          [horizontal ? "width" : "height"]: `${size}px`,
+        }}
+      />
+    );
+  } else {
+    return <div style={{ display: "flex", flex: `${flex} ${size ?? 0}px` }} />;
+  }
+};
+
+export default Spacer;
